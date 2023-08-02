71f0303907071fe79c4d79c44b598a4a2b4d40f0
Valentin Minkovich <valentino111@gmail.com>
2023-08-02 13:14:15 +0300

    Init commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const tasksInit = [
+  { id: 1, text: "Task 1", completed: false },
+  { id: 2, text: "Task 2", completed: true },
+  { id: 3, text: "Task 3", completed: false },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoApp></TodoApp>
     </div>
   );
 }
 
-export default App;
+function TodoApp() {
+  const [tasks, setTasks] = useState(tasksInit);
+
+  function handleAddTask(task) {
+    setTasks((tasks) => [...tasks, task]);
+  }
+
+  function handleDeleteTask(id) {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  function handleToggleTask(id) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
+  return (
+    <div className="todo-app">
+      <h1> ✔️ My Todo List</h1>
+      <TaskForm onTaskAdd={handleAddTask}></TaskForm>
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onToggleTask={handleToggleTask}
+      ></TaskList>
+    </div>
+  );
+}
+
+function TaskForm({ onTaskAdd }) {
+  const [taskText, setTaskText] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!taskText) return;
+
+    const newTask = { id: Date.now(), text: taskText, completed: false };
+    onTaskAdd(newTask);
+    setTaskText("");
+  }
+
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="task-input"
+        value={taskText}
+        onChange={(e) => setTaskText(e.target.value)}
+      ></input>
+
+      <button className="add-button">Add Task</button>
+    </form>
+  );
+}
+
+function TaskList({ tasks, onDeleteTask, onToggleTask }) {
+  return (
+    <div className="task-list">
+      <ul>
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onDeleteTask={onDeleteTask}
+            onToggleTask={onToggleTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function TaskItem({ task, onDeleteTask, onToggleTask }) {
+  return (
+    <li className="task-item">
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => onToggleTask(task.id)}
+      />
+
+      <p className={`task-text ${task.completed ? "completed" : ""}`}>
+        {task.text}
+      </p>
+
+      <div className="task-buttons">
+        <button
+          className="complete-button"
+          onClick={() => onToggleTask(task.id)}
+        >
+          Complete
+        </button>
+
+        <button className="delete-button" onClick={() => onDeleteTask(task.id)}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
+}
